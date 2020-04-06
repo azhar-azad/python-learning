3a5e551e94722d4a2eb9c909e6981470c639a4dc
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-04-06 16:09:48 +0600

    modules

---
+++ python-basics/src/py-level-two/modules_packages.py
@@ -0,0 +1,21 @@
+# Modules and Packages
+
+## Using Module(1):
+# import mymodule
+#
+# mymodule.func_in_module()
+
+## Using Module(2)
+# import mymodule as mm
+#
+# mm.func_in_module()
+
+## Using Module(3): partial import
+# from mymodule import func_in_module
+#
+# func_in_module()
+
+## Using Module(4): all import
+from mymodule import *
+
+func_in_module()
---
+++ python-basics/src/py-level-two/mymodule.py
@@ -0,0 +1,2 @@
+def func_in_module():
+    print("I AM INSIDE MY_MODULE.PY")
